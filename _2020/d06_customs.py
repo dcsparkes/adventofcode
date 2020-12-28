19464c09d42872b5f8283cb130de64a52ff86c83
dcsparkes <dcsparkes@gmail.com>
2020-12-28 00:47:57 +0000

    Migrate day 6 task to consistent file structure.
    Fix problems caused by over-enthusiastic stray refactoring of filenames by
    PyCharm.

--- _2020/d06_customs.py
+++ _2020/d06_customs.py
@@ -1,3 +1,6 @@
+"""
+https://adventofcode.com/2020/day/6
+"""
 def getAnswers(fileName):
     uniqueAnswers = set()
     sharedAnswers = set()
@@ -5,25 +8,19 @@ def getAnswers(fileName):
     with open(fileName) as inFile:
         for line in inFile:
             newAnswers = set(line.strip())
-            # print(newAnswers)
 
             if not newAnswers: # Empty line
-                # print("END: unique: {} shared: {} new: {}".format(uniqueAnswers, sharedAnswers, newAnswers))
                 yield (uniqueAnswers, sharedAnswers)
                 uniqueAnswers.clear()
                 sharedAnswers.clear()
 
             elif not uniqueAnswers: # First Person in Group
-                # print("FIRST: unique: {} shared: {} new: {}".format(uniqueAnswers, sharedAnswers, newAnswers))
                 uniqueAnswers = newAnswers
                 sharedAnswers = newAnswers
-                # print("unique: {} shared: {}".format(uniqueAnswers, sharedAnswers))
 
             else:
-                # print("unique: {} shared: {} new: {}".format(uniqueAnswers, sharedAnswers, newAnswers))
                 uniqueAnswers = uniqueAnswers.union(newAnswers)
                 sharedAnswers = sharedAnswers.intersection(newAnswers)
-                # print("unique: {} shared: {}".format(uniqueAnswers, sharedAnswers))
 
     if len(uniqueAnswers):
          yield (uniqueAnswers, sharedAnswers)
@@ -45,17 +42,7 @@ def countSharedAnswers(fileName):
         yield len(groupAnswers)
 
 
-# Unittest functions
-def uniqueAnswersOld(fileName):
-    chars = set()
-    with open(fileName) as inFile:
-        for line in inFile:
-            cs = line.strip()
-            if len(cs):
-                for c in cs:
-                    chars.add(c)
-            elif len(chars):
-                yield chars
-                chars.clear()
-    if len(chars):
-        yield chars
+if __name__ == '__main__':
+    fileName = "../input/input2020_06a.txt"
+    print("Part One Answer: {}".format(sum(countUniqueAnswers(fileName))))
+    print("Part Two Answer: {}".format(sum(countSharedAnswers(fileName))))
--- test/test_customs.py
+++ test/test_customs.py
@@ -1,32 +1,48 @@
-import customs
 import unittest
+from _2020 import d06_customs
+
+
+# Unittest functions
+def uniqueAnswersOld(fileName):
+    chars = set()
+    with open(fileName) as inFile:
+        for line in inFile:
+            cs = line.strip()
+            if len(cs):
+                for c in cs:
+                    chars.add(c)
+            elif len(chars):
+                yield chars
+                chars.clear()
+    if len(chars):
+        yield chars
 
 
 class TestCustoms(unittest.TestCase):
     def setUp(self):
-        self.inputFile = "input.txt"
-        self.testFile = "test.txt"
+        self.inputFile = "../input/input2020_06a.txt"
+        self.testFile = "../input/test2020_06a.txt"
 
     def test_countSharedAnswers_testFile_counts(self):
-        self.assertEqual([3, 0, 1, 1, 1], list(customs.countSharedAnswers(self.testFile)))
+        self.assertEqual([3, 0, 1, 1, 1], list(d06_customs.countSharedAnswers(self.testFile)))
 
     def test_countSharedAnswers_testFile_total(self):
-        self.assertEqual(6, sum(customs.countSharedAnswers(self.testFile)))
+        self.assertEqual(6, sum(d06_customs.countSharedAnswers(self.testFile)))
 
     def test_countSharedAnswers_inputFile_total(self):
-        self.assertEqual(3316, sum(customs.countSharedAnswers(self.inputFile)))
+        self.assertEqual(3316, sum(d06_customs.countSharedAnswers(self.inputFile)))
 
     def test_countUniqueAnswers_testFile_counts(self):
-        self.assertEqual([3, 3, 3, 1, 1], list(customs.countUniqueAnswers(self.testFile)))
+        self.assertEqual([3, 3, 3, 1, 1], list(d06_customs.countUniqueAnswers(self.testFile)))
 
     def test_countUniqueAnswers_testFile_total(self):
-        self.assertEqual(11, sum(customs.countUniqueAnswers(self.testFile)))
+        self.assertEqual(11, sum(d06_customs.countUniqueAnswers(self.testFile)))
 
     def test_countUniqueAnswers_inputFile_total(self):
-        self.assertEqual(6726, sum(customs.countUniqueAnswers(self.inputFile)))
+        self.assertEqual(6726, sum(d06_customs.countUniqueAnswers(self.inputFile)))
 
     def test_getAnswers_testFile(self):
-        answers = customs.getAnswers(self.testFile)
+        answers = d06_customs.getAnswers(self.testFile)
         self.assertEqual((set(['a', 'b', 'c']), set(['a', 'b', 'c'])), next(answers), 0)
         self.assertEqual((set(['a', 'b', 'c']), set()), next(answers), 1)
         self.assertEqual((set(['a', 'b', 'c']), set(['a'])), next(answers), 2)
@@ -34,7 +50,7 @@ class TestCustoms(unittest.TestCase):
         self.assertEqual((set(['b']), set(['b'])), next(answers), 4)
 
     def test_sharedAnswers_testFile(self):
-        answers = customs.sharedAnswers(self.testFile)
+        answers = d06_customs.sharedAnswers(self.testFile)
         self.assertEqual(set(['a', 'b', 'c']), next(answers), 0)
         self.assertEqual(set(), next(answers), 1)
         self.assertEqual(set(['a']), next(answers), 2)
@@ -42,7 +58,7 @@ class TestCustoms(unittest.TestCase):
         self.assertEqual(set(['b']), next(answers), 4)
 
     def test_uniqueAnswers_testFile(self):
-        answers = customs.uniqueAnswers(self.testFile)
+        answers = d06_customs.uniqueAnswers(self.testFile)
         self.assertEqual(set(['a', 'b', 'c']), next(answers))
         self.assertEqual(set(['a', 'b', 'c']), next(answers))
         self.assertEqual(set(['a', 'b', 'c']), next(answers))
@@ -50,14 +66,14 @@ class TestCustoms(unittest.TestCase):
         self.assertEqual(set(['b']), next(answers))
 
     def test_uniqueAnswers_alternateImplementation_inputFile(self):
-        answers = customs.uniqueAnswers(self.inputFile)
-        testAnswers = customs.uniqueAnswersOld(self.inputFile)
+        answers = d06_customs.uniqueAnswers(self.inputFile)
+        testAnswers = uniqueAnswersOld(self.inputFile)
         for i in range(5):
             self.assertEqual(next(testAnswers), next(answers), i)
 
     def test_uniqueAnswers_alternateImplementation_testFile(self):
-        answers = customs.uniqueAnswers(self.testFile)
-        testAnswers = customs.uniqueAnswersOld(self.testFile)
+        answers = d06_customs.uniqueAnswers(self.testFile)
+        testAnswers = uniqueAnswersOld(self.testFile)
         for i in range(5):
             self.assertEqual(next(testAnswers), next(answers), i)
 

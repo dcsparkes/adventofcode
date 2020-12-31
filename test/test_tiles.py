3921931b26850af2fe78fd45a382c5e8cfcba4bc
dcsparkes <dcsparkes@gmail.com>
2020-12-31 20:44:38 +0000

    Implement Day 20 (Jurassic Jigsaw).
    Complete parts 1 & 2, the last stars

--- test/test_tiles.py
+++ test/test_tiles.py
@@ -1,21 +1,222 @@
+"""
+https://adventofcode.com/2020/day/20
+"""
 import unittest
 from tiles import tiles
 
 
-class TestTiles(unittest.TestCase):
-    tile1 = ["Tile 1", "..#", ".##", "..#"]
+class TestDealer(unittest.TestCase):
+    fInput = "input2020_20a.txt"
+    fTest = "test2020_20a.txt"
+    knownPuzzleSolution = [".#.#..#.##...#.##..#####", "###....#.#....#..#......", "##.##.###.#.#..######...",
+                           "###.#####...#.#####.#..#", "##.#....#.##.####...#.##", "...########.#....#####.#",
+                           "....#..#...##..#.#.###..", ".####...#..#.....#......", "#..#.##..#..###.#.##....",
+                           "#.####..#.####.#.#.###..", "###.#.#...#.######.#..##", "#.####....##..########.#",
+                           "##..##.#...#...#.#.#.#..", "...#..#..#.#.##..###.###", ".#.#....#.##.#...###.##.",
+                           "###.#...#..#.##.######..", ".#.#.###.##.##.#..#.##..", ".####.###.#...###.#..#.#",
+                           "..#.#..#..#.#.#.####.###", "#..####...#.#.#.###.###.", "#####..#####...###....##",
+                           "#.##..#..#...#..####...#", ".#.###..##..##..####.##.", "...###...##...#...#..###"]
+
+    serpentPattern = ["                  # ", "#    ##    ##    ###", " #  #  #  #  #  #   "]
+
+    def test_init_fTest_dims(self):
+        d = tiles.Dealer(self.fTest)
+        self.assertEqual(9, d.count)
+        self.assertEqual((3, 3), d.dims)
+
+    def test_init_corners_fInput(self):
+        d = tiles.Dealer(self.fInput)
+        self.assertEqual(45443966642567, d.calculateCornerMultiple())
+
+    def test_init_corners_fTest(self):
+        d = tiles.Dealer(self.fTest)
+        self.assertEqual(20899048083289, d.calculateCornerMultiple())
+
+    def test_assemblePuzzle_fTest_count(self):
+        d = tiles.Dealer(self.fTest)
+        d.assemblePuzzle()
+        self.assertEqual(303, d.puzzle.countSymbol('#'))
+
+    def test_assemblePuzzle_fTest_dims(self):
+        d = tiles.Dealer(self.fTest)
+        tileDims = d.tiles[0].dims
+        puzzleDims = d.dims
+        finalDims = ((tileDims[0] - 2) * puzzleDims[0], (tileDims[1] - 2) * puzzleDims[1])
+        d.assemblePuzzle()
+        self.assertEqual(finalDims, d.puzzle.dims)
+
+    def test_assemblePuzzle_fInput_count(self):
+        d = tiles.Dealer(self.fInput)
+        d.assemblePuzzle()
+        self.assertEqual(1922, d.puzzle.countSymbol('#'))
+
+    def test_assemblePuzzle_fInput_dims(self):
+        d = tiles.Dealer(self.fInput)
+        tileDims = d.tiles[0].dims
+        puzzleDims = d.dims
+        finalDims = ((tileDims[0] - 2) * puzzleDims[0], (tileDims[1] - 2) * puzzleDims[1])
+        d.assemblePuzzle()
+        self.assertEqual(finalDims, d.puzzle.dims)
+
+    def test_assemblePuzzle_fTest_knownGoodAssembly(self):
+        d = tiles.Dealer(self.fTest)
+        d.assemblePuzzle()
+        matched = False
+        for t in d.puzzle.spin():
+            matched |= t.tile == self.knownPuzzleSolution
+        self.assertTrue(matched)
+
+    def test_findSerpents_fTest_count(self):
+        d = tiles.Dealer(self.fTest)
+        d.assemblePuzzle()
+        self.assertEqual(2, len(d.findSerpents()))
+
+    def test_findSerpents_fInput_count(self):
+        d = tiles.Dealer(self.fInput)
+        d.assemblePuzzle()
+        self.assertEqual(21, len(d.findSerpents()))
+
+
+
+
+class TestTile(unittest.TestCase):
+    tile1 = ["Tile 1:", "..#", ".##", "..#"]
     t1Sides = [(1, 4), (7, 7), (1, 4), (0, 0)]
 
-    tile2 = ["Tile 2", "...#", "..##", "###.", ".###"]
+    tile2 = ["Tile 2:", "...#", "..##", "###.", ".###"]
     t2Sides = [(1, 8), (11, 13), (7, 14), (2, 4)]
+    serpentsLocated = [".####...#####..#...###..",
+                       "#####..#..#.#.####..#.#.",
+                       ".#.#...#.###...#.##.O#..",
+                       "#.O.##.OO#.#.OO.##.OOO##",
+                       "..#O.#O#.O##O..O.#O##.##",
+                       "...#.#..##.##...#..#..##",
+                       "#.##.#..#.#..#..##.#.#..",
+                       ".###.##.....#...###.#...",
+                       "#.####.#.#....##.#..#.#.",
+                       "##...#..#....#..#...####",
+                       "..#.##...###..#.#####..#",
+                       "....#.##.#.#####....#...",
+                       "..##.##.###.....#.##..#.",
+                       "#...#...###..####....##.",
+                       ".#.##...#.##.#.#.###...#",
+                       "#.###.#..####...##..#...",
+                       "#.###...#.##...#.##O###.",
+                       ".O##.#OO.###OO##..OOO##.",
+                       "..O#.O..O..O.#O##O##.###",
+                       "#.#..##.########..#..##.",
+                       "#.#####..#.#...##..#....",
+                       "#....##..#.#########..##",
+                       "#...#.....#..##...###.##",
+                       "#..###....##.#...##.##.#"]
 
-    def test_init_sideIDcalculation_3x3(self):
+    knownPuzzleSolutionOriented = [".####...#####..#...###..",
+                                   "#####..#..#.#.####..#.#.",
+                                   ".#.#...#.###...#.##.##..",
+                                   "#.#.##.###.#.##.##.#####",
+                                   "..##.###.####..#.####.##",
+                                   "...#.#..##.##...#..#..##",
+                                   "#.##.#..#.#..#..##.#.#..",
+                                   ".###.##.....#...###.#...",
+                                   "#.####.#.#....##.#..#.#.",
+                                   "##...#..#....#..#...####",
+                                   "..#.##...###..#.#####..#",
+                                   "....#.##.#.#####....#...",
+                                   "..##.##.###.....#.##..#.",
+                                   "#...#...###..####....##.",
+                                   ".#.##...#.##.#.#.###...#",
+                                   "#.###.#..####...##..#...",
+                                   "#.###...#.##...#.######.",
+                                   ".###.###.#######..#####.",
+                                   "..##.#..#..#.#######.###",
+                                   "#.#..##.########..#..##.",
+                                   "#.#####..#.#...##..#....",
+                                   "#....##..#.#########..##",
+                                   "#...#.....#..##...###.##",
+                                   "#..###....##.#...##.##.#"]
+
+    knownPuzzleSolutionUnoriented = [".#.#..#.##...#.##..#####",
+                                     "###....#.#....#..#......",
+                                     "##.##.###.#.#..######...",
+                                     "###.#####...#.#####.#..#",
+                                     "##.#....#.##.####...#.##",
+                                     "...########.#....#####.#",
+                                     "....#..#...##..#.#.###..",
+                                     ".####...#..#.....#......",
+                                     "#..#.##..#..###.#.##....",
+                                     "#.####..#.####.#.#.###..",
+                                     "###.#.#...#.######.#..##",
+                                     "#.####....##..########.#",
+                                     "##..##.#...#...#.#.#.#..",
+                                     "...#..#..#.#.##..###.###",
+                                     ".#.#....#.##.#...###.##.",
+                                     "###.#...#..#.##.######..",
+                                     ".#.#.###.##.##.#..#.##..",
+                                     ".####.###.#...###.#..#.#",
+                                     "..#.#..#..#.#.#.####.###",
+                                     "#..####...#.#.#.###.###.",
+                                     "#####..#####...###....##",
+                                     "#.##..#..#...#..####...#",
+                                     ".#.###..##..##..####.##.",
+                                     "...###...##...#...#..###"]
+
+    serpentPattern = ["                  # ",
+                      "#    ##    ##    ###",
+                      " #  #  #  #  #  #   "]
+
+    def test_init_sideID_calculation_3x3(self):
         t = tiles.Tile(self.tile1)
         self.assertEqual(self.t1Sides, t.sideIDs)
 
-    def test_init_sideIDcalculation_4x4(self):
+    def test_init_sideID_calculation_4x4(self):
         t = tiles.Tile(self.tile2)
         self.assertEqual(self.t2Sides, t.sideIDs)
+
+    def test_count(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        self.assertEqual(303, tile.countSymbol('#'))
+
+    def test_findPattern_knownGoodAssembly_oriented_count(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        matches = [m for m in tile._findPatternMatches(self.serpentPattern)]
+        self.assertTrue(2, len(matches))
+
+    def test_findPattern_knownGoodAssembly_unoriented_count(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionUnoriented)
+        tile = tiles.Tile(buffer)
+        matches = [m for m in tile._findPatternMatches(self.serpentPattern)]
+        self.assertTrue(2, len(matches))
+
+    def test_findPatternMatches_knownGoodAssembly_count(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        matches = [m for m in tile._findPatternMatches(self.serpentPattern)]
+        self.assertTrue(2, len(matches))
+
+    def test_findPatternMatches_knownGoodAssembly_match(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        matches = [m for m in tile._findPatternMatches(self.serpentPattern)]
+        self.assertEqual(set([(2, 2), (1, 16)]), set(matches))
+
+    def test_patternMatch_knownGoodAssembly_pos1(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        self.assertTrue(tile._patternMatch((2, 2), self.serpentPattern))
+
+    def test_patternMatch_knownGoodAssembly_pos2(self):
+        buffer = ["0"]
+        buffer.extend(self.knownPuzzleSolutionOriented)
+        tile = tiles.Tile(buffer)
+        self.assertTrue(tile._patternMatch((1, 16), self.serpentPattern))
 
     def test_rotate90_corners_3x3(self):
         t = tiles.Tile(self.tile1)
@@ -31,6 +232,18 @@ class TestTiles(unittest.TestCase):
         self.assertEqual(orig[-1][0], newT[0][0], "Bottom left -> top left")
         self.assertEqual(orig[-1][1], newT[1][0], "Bottom middle -> middle left")
         self.assertEqual(orig[-1][-1], newT[-1][0], "Bottom right -> bottom left")
+
+    def test_rotate90_edgeIDs_3x3(self):
+        t = tiles.Tile(self.tile1)
+        t.rotate90()
+        expected = [self.t1Sides[3], self.t1Sides[0], self.t1Sides[1], self.t1Sides[2]]
+        self.assertEqual(expected, t.sideIDs)
+
+    def test_rotate90_edgeIDs_4x4(self):
+        t = tiles.Tile(self.tile2)
+        t.rotate90()
+        expected = [self.t2Sides[3], self.t2Sides[0], self.t2Sides[1], self.t2Sides[2]]
+        self.assertEqual(expected, t.sideIDs)
 
     def test_rotate90_corners_4x4(self):
         t = tiles.Tile(self.tile2)
@@ -53,12 +266,36 @@ class TestTiles(unittest.TestCase):
             t.rotate90()
         self.assertEqual(orig, str(t))
 
+    def test_rotate180_edgeIDs_3x3(self):
+        t = tiles.Tile(self.tile1)
+        t.rotate180()
+        expected = [self.t1Sides[2], self.t1Sides[3], self.t1Sides[0], self.t1Sides[1]]
+        self.assertEqual(expected, t.sideIDs)
+
+    def test_rotate180_edgeIDs_4x4(self):
+        t = tiles.Tile(self.tile2)
+        t.rotate180()
+        expected = [self.t2Sides[2], self.t2Sides[3], self.t2Sides[0], self.t2Sides[1]]
+        self.assertEqual(expected, t.sideIDs)
+
     def test_rotate180_full_rotation_360degrees(self):
         t = tiles.Tile(self.tile1)
         orig = str(t)
         for i in range(2):
             t.rotate180()
         self.assertEqual(orig, str(t))
+
+    def test_rotate270_edgeIDs_3x3(self):
+        t = tiles.Tile(self.tile1)
+        t.rotate270()
+        expected = [self.t1Sides[1], self.t1Sides[2], self.t1Sides[3], self.t1Sides[0]]
+        self.assertEqual(expected, t.sideIDs)
+
+    def test_rotate270_edgeIDs_4x4(self):
+        t = tiles.Tile(self.tile2)
+        t.rotate270()
+        expected = [self.t2Sides[1], self.t2Sides[2], self.t2Sides[3], self.t2Sides[0]]
+        self.assertEqual(expected, t.sideIDs)
 
     def test_rotate270_full_rotation_1080degrees(self):
         t = tiles.Tile(self.tile1)
@@ -72,7 +309,7 @@ class TestTiles(unittest.TestCase):
         t2 = tiles.Tile(self.tile1)
         for i in range(2):
             t1.rotate90()
-            print(t1)
+            # print(t1)
         t2.rotate180()
         self.assertEqual(str(t1), str(t2))
 
@@ -100,6 +337,18 @@ class TestTiles(unittest.TestCase):
         t2.rotate270()
         self.assertEqual(str(t1), str(t2))
 
+    def test_flipHorizontal_edgeIDs_3x3(self):
+        t = tiles.Tile(self.tile1)
+        t.flipHorizontal()
+        expected = [self.t1Sides[0], self.t1Sides[3], self.t1Sides[2], self.t1Sides[1]]
+        self.assertEqual(expected, t.sideIDs)
+
+    def test_flipHorizontal_edgeIDs_4x4(self):
+        t = tiles.Tile(self.tile2)
+        t.flipHorizontal()
+        expected = [self.t2Sides[0], self.t2Sides[3], self.t2Sides[2], self.t2Sides[1]]
+        self.assertEqual(expected, t.sideIDs)
+
     def test_flipHorizontal_reversible(self):
         t = tiles.Tile(self.tile1)
         orig = str(t)
@@ -107,12 +356,25 @@ class TestTiles(unittest.TestCase):
             t.flipHorizontal()
         self.assertEqual(orig, str(t))
 
+    def test_flipVertical_edgeIDs_3x3(self):
+        t = tiles.Tile(self.tile1)
+        t.flipVertical()
+        expected = [self.t1Sides[2], self.t1Sides[1], self.t1Sides[0], self.t1Sides[3]]
+        self.assertEqual(expected, t.sideIDs)
+
+    def test_flipVertical_edgeIDs_4x4(self):
+        t = tiles.Tile(self.tile2)
+        t.flipVertical()
+        expected = [self.t2Sides[2], self.t2Sides[1], self.t2Sides[0], self.t2Sides[3]]
+        self.assertEqual(expected, t.sideIDs)
+
     def test_flipVertical_reversible(self):
         t = tiles.Tile(self.tile1)
         orig = str(t)
         for i in range(2):
             t.flipVertical()
         self.assertEqual(orig, str(t))
+
 
 if __name__ == '__main__':
     unittest.main()
--- tiles/tiles.py
+++ tiles/tiles.py
@@ -1,15 +1,17 @@
 """
 Play a jigsaw of square tiles
+https://adventofcode.com/2020/day/20
 """
+from base import base
 
 
 class Tile:
     def __init__(self, buffer):
-        # print(buffer)
-        self.id = int(buffer[0][5:])
+        self.id = int(buffer[0].strip('Tile :'))
         self.tile = buffer[1:]
         self.dims = (len(self.tile), len(self.tile[0]))
         self.sideIDs = [None] * 4
+        self.edges = []
         self._calculateSides()
 
     def __repr__(self):
@@ -18,20 +20,6 @@ class Tile:
     def __str__(self):
         return '\n'.join(self.tile)
 
-    @staticmethod
-    def sideToIDs(code):
-        """
-        Return the int value and its matching complement (i.e. reversed value) sorted in value order for comparisons.
-        :param code: The side as a string of '#' and '.' chars,
-        :return:    To sort or not to sort?  Sorting aids matching and is flip independent, but not sorting allows
-                    state information about chirality to be maintained.  Sorting for simplicity, but chirality might
-                    need to be encoded separately.
-        """
-        bin = code.replace('.', '0').replace('#', '1')
-        value = int(bin, 2)
-        complement = int(bin[::-1], 2)
-        return tuple(sorted([value, complement]))
-
     def _calculateSides(self):
         self.sideIDs[0] = self.sideToIDs(self.tile[0])
         self.sideIDs[2] = self.sideToIDs(self.tile[-1])
@@ -39,11 +27,115 @@ class Tile:
         self.sideIDs[3] = self.sideToIDs(zipped[0])
         self.sideIDs[1] = self.sideToIDs(zipped[-1])
 
+    def align(self, topID, leftID):
+        funcs = [None, self.rotate270, self.rotate180, self.rotate90]
+        if topID:
+            orientation = self.sideIDs.index(topID)
+            if orientation:
+                funcs[orientation]()  # Rotate topID to top
+
+            if leftID is None:
+                if self.sideIDs[3] not in self.edges:
+                    self.flipHorizontal()
+                    if self.sideIDs[3] not in self.edges:
+                        raise ValueError("Invalid Rotation: {}, {}".format(topID, leftID))
+
+            elif self.sideIDs[3] != leftID:
+                self.flipHorizontal()
+                if self.sideIDs[3] != leftID:
+                    raise ValueError("Invalid Rotation: {}, {}".format(topID, leftID))
+        else:  # if not topID
+            orientation = (1 + self.sideIDs.index(leftID)) % 4
+            if orientation:
+                funcs[orientation]()  # Rotate leftID to left
+
+            if self.sideIDs[0] not in self.edges:
+                self.flipVertical()
+                if self.sideIDs[0] not in self.edges:
+                    raise ValueError("Invalid Rotation: {}, {}".format(topID, leftID))
+
+    @staticmethod
+    def concatenateTileRow(tileRow, delimiter='  ') -> str:  # Mainly for debug
+        return '\n'.join([delimiter.join(row) for row in zip(*[t.tile for t in tileRow])])
+
+    @staticmethod
+    def concatenateTrimmedTileRow(tileRow) -> list:
+        """
+        Strip the border and concatenate the tiles into a list of lines, 1 per height of the tile
+        :param tileRow: A list of Tile objects
+        :return: A list of str
+        """
+        return [''.join([str(tr[1:-1]) for tr in trs]) for trs in list(zip(*[t.tile for t in tileRow]))[1:-1]]
+
+    def _translatePatternToRegExPattern(self, pattern):
+        """
+
+        :param pattern: lines
+        :return: regular expression
+        """
+        regExes = []
+        for line in pattern:
+            line.replace(' ', '.')
+            regExes.append(line.replace(' ', '.'))
+
+    def _patternMatch(self, coords, pattern):
+        """
+        Check if the tile matches the pattern at the coordinates given.
+
+        :param coords: x, y coordinate within tile.
+        :param pattern:
+        :return:
+        """
+        patWidth = len(pattern[0])
+        patHeight = len(pattern)
+        x0, y0 = coords
+        for y1 in range(patHeight):
+            for x1 in range(patWidth):
+                if pattern[y1][x1] == '#':
+                    if self.tile[y0 + y1][x0 + x1] != '#':
+                        return False
+        return True
+
+    def _findPatternMatches(self, pattern):
+        """
+        On a particular orientation of the tile find matches to the pattern.
+        :param pattern:
+        :return:
+        """
+        patWidth = len(pattern[0])
+        patHeight = len(pattern)
+        for x in range(self.dims[0] - patWidth):
+            for y in range(self.dims[1] - patHeight):
+                if self._patternMatch((x, y), pattern):
+                    yield (x, y)
+
+    def countSymbol(self, symbol):
+        return sum([line.count(symbol) for line in self.tile])
+
+
+    def findPattern(self, pattern):
+        """
+        Find instances of the pattern on the tile.  Cycle through every orientation and see if there are any matches.
+        Assumptions:    The pattern matches only in one orientation.
+        :param pattern:
+        :return:
+        """
+        for t in self.spin():
+            matches = [match for match in t._findPatternMatches(pattern)]
+            if matches:
+                return matches
+
     def flipHorizontal(self):
         self.tile = [row[::-1] for row in self.tile]
+        swap = self.sideIDs[1]
+        self.sideIDs[1] = self.sideIDs[3]
+        self.sideIDs[3] = swap
 
     def flipVertical(self):
         self.tile.reverse()
+        swap = self.sideIDs[2]
+        self.sideIDs[2] = self.sideIDs[0]
+        self.sideIDs[0] = swap
 
     def rotate90(self):
         self.tile = [''.join(row) for row in zip(*reversed(self.tile))]
@@ -57,33 +149,161 @@ class Tile:
         self.tile = [''.join(row) for row in reversed(list(zip(*self.tile)))]
         self.sideIDs = self.sideIDs[1:] + self.sideIDs[:1]
 
-    def __str__(self):
-        return '\n'.join(self.tile)
+    @staticmethod
+    def sideToIDs(code):
+        """
+        Return the int value and its matching complement (i.e. reversed value) sorted in value order for comparisons.
+        :param code: The side as a string of '#' and '.' chars,
+        :return:    To sort or not to sort?  Sorting aids matching and is flip independent, but not sorting allows
+                    state information about chirality to be maintained.  Sorting for simplicity, but chirality might
+                    need to be encoded separately.
+        """
+        binary = code.replace('.', '0').replace('#', '1')
+        value = int(binary, 2)
+        complement = int(binary[::-1], 2)
+        return tuple(sorted([value, complement]))
+
+    def spin(self):
+        """
+        Rotate and flip to all 8 possible orientations
+        :return:
+        """
+        for f in range(2):
+            for i in range(4):
+                yield self
+                self.rotate90()
+            self.flipHorizontal()
 
 
 class Dealer:
-    def __init__(self, fileName):
-        self.playGame(fileName)
-        self.tiles = []
+    puzzle: Tile
 
-    def readTiles(fileName):
+    def __init__(self, fileName):
+        self.tiles = []
+        self.count = 0
+        self.dims = None
+        self.sideIndices = {}
+        self.cornerResult = None
+        self.puzzle = None
+        self._setup(fileName)
+
+    @staticmethod
+    def _findDims(num):
+        """
+        Find the dimensions of the puzzle in tiles.  Assume square.
+        :param num: Number of tiles.
+        :return:  Tuple of (x, y) dimensions.
+        """
+        root = int(num ** 0.5)
+        return (root, root)
+
+    def _findEdges(self):
+        self.corners = []
+        for ind in self.sideIndices:
+            if len(self.sideIndices[ind]) == 1:  # Edge
+                tile = self.sideIndices[ind][0]
+                tile.edges.append(ind)
+                if len(tile.edges) == 2:
+                    self.corners.append(tile)
+
+    def _findNextTile(self, prev, tileId):
+        pair = self.sideIndices[tileId]
+        if pair[0] == prev:
+            return pair[1]
+        else:
+            return pair[0]
+
+    @staticmethod
+    def _readTiles(fileName):
         tileBuffer = []
         for line in base.getInputLines(fileName):
-            print(line)
+            # print(line)
             if not line:
                 pass
 
-            elif not tileBuffer and line[:4] == "Tile":
+            elif line[:4] == "Tile" and tileBuffer:
                 yield Tile(tileBuffer)
                 tileBuffer.clear()
-                tileBuffer.append(line)
+                tileBuffer.append(line.strip())
 
             else:
-                tileBuffer.append(line)
+                tileBuffer.append(line.strip())
 
         yield Tile(tileBuffer)
 
-    def playGame(fileName):
-        for tile in readTiles(fileName):
-            self.tiles.insert(tile)
-            print(tile)
+    def _registerSides(self, tile):
+        for index in tile.sideIDs:
+            if index not in self.sideIndices:
+                self.sideIndices[index] = [tile]
+            else:
+                self.sideIndices[index].append(tile)
+
+    def _setup(self, fileName):
+        for tile in self._readTiles(fileName):
+            self.tiles.append(tile)
+            self._registerSides(tile)
+            self.count += 1
+
+        self.dims = self._findDims(self.count)  # for now assume square
+        self._findEdges()
+
+    def assemblePuzzle(self):
+        """
+        Put the tiles together.  Because the IDs are unique chirality is not an issue.  Arbitrarily pick a corner
+        and puzzle orientation, orient up and left, then proceed column by column, row by row, orienting up and
+        left.  Requires two functions in Tile: align() to rotate and flip the tile to match orientation and trim()
+        to take off the edges.
+        Additional function to put the tiles together so that they can be assembled into one big tile.
+
+        :return: assembled puzzle with edges removed
+        """
+        start = self.corners[0]
+        # print(start.sideIDs)
+        # print(start.edges)
+        start.align(*start.edges)
+        # print(start.sideIDs)
+        # print(start.edges)
+
+        # Populate Top Edge
+        puzzle = [[start]]
+        row = 0
+        for col in range(1, self.dims[0]):
+            prevTile = puzzle[row][-1]
+            leftID = prevTile.sideIDs[1]
+            nextTile = self._findNextTile(prevTile, leftID)
+            nextTile.align(topID=None, leftID=leftID)
+            puzzle[row].append(nextTile)
+        # Populate Left Edge
+        col = 0
+        for row in range(1, self.dims[1]):
+            prevTile = puzzle[-1][col]
+            topID = prevTile.sideIDs[2]
+            nextTile = self._findNextTile(prevTile, topID)
+            nextTile.align(topID=topID, leftID=None)
+            puzzle.append([nextTile])
+        # Populate main body
+        for row in range(1, self.dims[1]):
+            for col in range(1, self.dims[0]):
+                prevTile = puzzle[row][-1]
+                aboveTileID = puzzle[row - 1][col].sideIDs[2]
+                leftID = prevTile.sideIDs[1]
+                nextTile = self._findNextTile(prevTile, leftID)
+                nextTile.align(topID=aboveTileID, leftID=leftID)
+                puzzle[row].append(nextTile)
+
+        tileDefBuffer = [str(self.calculateCornerMultiple())]
+        for tileRow in puzzle:
+            tileDefBuffer.extend(Tile.concatenateTrimmedTileRow(tileRow))
+        self.puzzle = Tile(tileDefBuffer)
+
+    def calculateCornerMultiple(self):
+        cornerResult = 1
+        if len(self.corners) == 4:
+            for corner in self.corners:
+                cornerResult *= corner.id
+        return cornerResult
+
+    def findSerpents(self):
+        serpentPattern = ["                  # ", "#    ##    ##    ###", " #  #  #  #  #  #   "]
+        count = self.puzzle.findPattern(serpentPattern)
+        return count

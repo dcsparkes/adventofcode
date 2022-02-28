0cf841cc5fa10f2e55b8bdd3830d1348d1a407dc
dcsparkes@gmail.com <cwBA44K4jAcqHQ5>
2022-02-28 04:24:43 +0000

    Implement day 2, task 2 of AoC 2021.

--- _2021/d02_dive.py
+++ _2021/d02_dive.py
@@ -7,33 +7,38 @@ import logging
 import sys
 
 
-def readInstructions(fileName):
-    """
-
-    :param fileName:
-    :return:
-    """
-    for line in base.getInputLines(fileName):
-        yield line
-
-
-def convertInstructionToVector(instruction):
-    direction, distance = instruction.split()
-
-    unitVectors = {"forward": (1, 0),
-                   "up": (0, -1),
-                   "down": (0, 1),
-                   }
-
-    vector = tuple([x * int(distance) for x in unitVectors[direction]])
-    logging.debug("{}(\"{}\"): direction: \"{}\", distance: {}, vector: {}".format(sys._getframe().f_code.co_name,
-                                                                       instruction, direction, distance, vector))
-    return vector
-
-
 def readVectors(fileName):
-    for instruction in readInstructions(fileName):
-        yield convertInstructionToVector(instruction)
+    for instruction in base.getInputLines(fileName):
+        direction, distance = instruction.split()
+
+        unitVectors = {"forward": (1, 0),
+                       "up": (0, -1),
+                       "down": (0, 1),
+                       }
+
+        vector = tuple([x * int(distance) for x in unitVectors[direction]])
+        logging.debug("{}(\"{}\"): direction: \"{}\", distance: {}, vector: {}".format(sys._getframe().f_code.co_name,
+                                                                                       instruction, direction, distance,
+                                                                                       vector))
+        yield vector
+
+
+def readVectorsV2(fileName):
+    aim = [1, 0]
+    for instruction in base.getInputLines(fileName):
+        direction, distString = instruction.split()
+
+        distance = int(distString)
+        if direction == "down":
+            aim[1] += distance
+        elif direction == "up":
+            aim[1] -= distance
+        elif direction == "forward":
+            moveVector = tuple([x * distance for x in aim])
+            yield moveVector
+        else:
+            logging.warning("{}(\"{}\"): Unrecognised direction: \"{}\"".format(sys._getframe().f_code.co_name,
+                                                                                fileName, direction))
 
 
 def addVectors(a, b):
@@ -46,23 +51,27 @@ def addVectors(a, b):
     """
     result = tuple([sum(x) for x in zip(a, b)])
     logging.debug("{}({}, {}): result: {}".format(sys._getframe().f_code.co_name,
-                                                                       a, b, result))
+                                                  a, b, result))
 
     return result
 
-def calcFinalPosition(fileName):
+
+def calcFinalPosition(fileName, readFunc=readVectors):
     position = (0, 0)
-    for move in readVectors(fileName):
+    for move in readFunc(fileName):
         position = addVectors(position, move)
     return position
 
-def calculateSweptRectangle(fileName, title):
-    position = calcFinalPosition(fileName)
+
+def calculateSweptRectangle(fileName, title, readFunc=readVectors):
+    position = calcFinalPosition(fileName, readFunc)
     sweptRectangleArea = position[0] * position[1]
     print("{}: End: {}, Area: {}".format(title, position, sweptRectangleArea))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="../log/2021_d02.log", encoding='utf-8', level=logging.DEBUG)
-    calculateSweptRectangle("../input/test2021_02a.txt", "Test 02a")
-    calculateSweptRectangle("../input/input2021_02a.txt", "Input 02a")
+    logging.basicConfig(filename="../log/2021_d02.log", encoding='utf-8', level=logging.INFO)
+    calculateSweptRectangle("../input/test2021_02a.txt", "Task 1: Test 02a")
+    calculateSweptRectangle("../input/input2021_02a.txt", "Task 1: Input 02a")
+    calculateSweptRectangle("../input/test2021_02a.txt", "Task 2: Test 02a", readVectorsV2)
+    calculateSweptRectangle("../input/input2021_02a.txt", "Task 2: Input 02a", readVectorsV2)

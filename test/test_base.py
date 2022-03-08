b09c92d1fb2c7c338213a138addc73966a21310d
dcsparkes@gmail.com <cwBA44K4jAcqHQ5>
2022-03-08 02:03:19 +0000

    Fix base tests.

--- test/test_base.py
+++ test/test_base.py
@@ -21,10 +21,16 @@ class TestBaseFunctions(unittest.TestCase):
         for lineTest in self.linesTest:
             self.assertEqual(lineTest, next(linesInput))
 
-    def test_getInts_base01(self):
+    def test_deprecatedFunction_getInts_base01(self):
         linesInput = base.getInts("../input/" + self.fileTest1)
         for lineTest in self.linesTest:
             self.assertEqual(int(lineTest), next(linesInput))
+
+    def test_getInputLines_asInt_base01(self):
+        linesInput = base.getInputLines(self.fileTest1, func=int)
+        for lineTest in self.linesTest:
+            self.assertEqual(int(lineTest), next(linesInput))
+
 
 
 if __name__ == '__main__':
